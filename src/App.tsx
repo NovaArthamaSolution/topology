e199069924fa5452aa255a58ec15e639e008f286
gilang-nugraha <128233782+gilang-nugraha@users.noreply.github.com>
2025-07-22 23:59:41 +0700

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,90 @@
+/* eslint-disable @typescript-eslint/ban-ts-comment */
+import React, { useState, useEffect } from "react";
+import Papa from "papaparse";
+import _ from "lodash";
+import Flow from "./Flow";
+import "./App.css";
+
+type RawData = {
+  [key: string]: string;
+};
+
+type Node = {
+  NodeId: number | null;
+  Nama_Aset: string;
+  No_Aset: string;
+  Class: string;
+  parentId: number | null;
+  position_tag: string;
+  view_icon: string;
+  grouping: string;
+  info: string;
+  level: number | null;
+  children?: Node[];
+};
+
+function App() {
+  const [data, setData] = useState<Node[]>([]);
+
+  const processAndCleanData = (rawData: RawData[]): Node[] => {
+    return rawData
+      .map(
+        (row): Node => ({
+          NodeId: row["NodeId"] ? parseInt(row["NodeId"], 10) : null,
+          Nama_Aset: row["Nama Aset"] || "",
+          No_Aset: row["No Aset"] || "",
+          Class: row["Class"] || "",
+          parentId: row["parentId"] ? parseInt(row["parentId"], 10) : null,
+          position_tag: row["position_tag"] || "",
+          view_icon: row["view_icon"] || "",
+          grouping: row["grouping"] || "",
+          info: row["info"] || "",
+          level: row["level"] ? parseInt(row["level"], 10) : null,
+        })
+      )
+      .filter((row) => row.NodeId !== null);
+  };
+
+  const groupDataByParentId = (cleanedData: Node[]): Node[] => {
+    const grouped = _.groupBy(cleanedData, (row) =>
+      row.parentId === null ? "root" : row.parentId
+    );
+
+    const buildHierarchy = (node: Node): Node => ({
+      ...node,
+      children: (grouped[node.NodeId!] || []).map((child) =>
+        buildHierarchy(child)
+      ),
+    });
+
+    return (grouped["root"] || []).map((node) => buildHierarchy(node));
+  };
+
+  useEffect(() => {
+    fetch("/data.csv")
+      .then((response) => {
+        if (!response.ok) throw new Error("Failed to fetch CSV");
+        return response.text();
+      })
+      .then((csvText) => {
+        Papa.parse(csvText, {
+          header: true,
+          skipEmptyLines: true,
+          transformHeader: (header) => header.trim().replace(/^"|"$/g, ""),
+          transform: (value) => value.trim().replace(/^"|"$/g, ""),
+          complete: (result: Papa.ParseResult<RawData>) => {
+            const cleanedData = processAndCleanData(result.data);
+            const groupedData = groupDataByParentId(cleanedData);
+            setData(groupedData);
+          },
+          error: (err: { message: string }) =>
+            console.log(`Error parsing CSV: ${err?.message}`),
+        });
+      })
+      .catch((err) => console.log(`Error fetching CSV: ${err.message}`));
+  }, []);
+  // @ts-expect-error
+  return <div id="app">{data && <Flow data={data} />}</div>;
+}
+
+export default App;
---
+++ src/lib/utils.ts
@@ -0,0 +1,532 @@
+import { clsx, type ClassValue } from "clsx";
+import { twMerge } from "tailwind-merge";
+
+export function cn(...inputs: ClassValue[]) {
+  return twMerge(clsx(inputs));
+}
+export const data = [
+  {
+    id: "-611386",
+    name: "Chicken",
+    data: "https://picsum.photos/seed/QFyKy86Fk/640/480",
+    childs: [
+      {
+        id: "131012",
+        name: "Car",
+        data: "https://picsum.photos/seed/xLOSaG8OiZ/640/480",
+        childs: [
+          {
+            id: "914241",
+            name: "Cheese",
+            data: "https://picsum.photos/seed/ariBD/640/480",
+            childs: [],
+          },
+          {
+            id: "703487",
+            name: "Salad",
+            data: "https://picsum.photos/seed/GiUUSl6J/640/480",
+            childs: [],
+          },
+          {
+            id: "155899",
+            name: "Hat",
+            data: "https://loremflickr.com/640/480?lock=2224184585355264",
+            childs: [],
+          },
+          {
+            id: "-290246",
+            name: "Shoes",
+            data: "https://picsum.photos/seed/LcewOrpNk/640/480",
+            childs: [],
+          },
+        ],
+      },
+      {
+        id: "422259",
+        name: "Cheese",
+        data: "https://picsum.photos/seed/vhK1DoR/640/480",
+        childs: [
+          {
+            id: "228553",
+            name: "Gloves",
+            data: "https://loremflickr.com/640/480?lock=5257463209132032",
+            childs: [],
+          },
+          {
+            id: "-175031",
+            name: "Chips",
+            data: "https://picsum.photos/seed/d4FAwPIA/640/480",
+            childs: [],
+          },
+          {
+            id: "-987041",
+            name: "Keyboard",
+            data: "https://picsum.photos/seed/sRddHuuY70/640/480",
+            childs: [],
+          },
+          {
+            id: "983152",
+            name: "Shirt",
+            data: "https://picsum.photos/seed/d43PnoSEK/640/480",
+            childs: [],
+          },
+        ],
+      },
+      {
+        id: "-128284",
+        name: "Chips",
+        data: "https://picsum.photos/seed/YoqcSTA/640/480",
+        childs: [
+          {
+            id: "461146",
+            name: "Towels",
+            data: "https://loremflickr.com/640/480?lock=3284741006557184",
+            childs: [],
+          },
+          {
+            id: "279284",
+            name: "Keyboard",
+            data: "https://loremflickr.com/640/480?lock=8857517220167680",
+            childs: [],
+          },
+          {
+            id: "169121",
+            name: "Chicken",
+            data: "https://picsum.photos/seed/o4CAWGlc/640/480",
+            childs: [],
+          },
+          {
+            id: "161997",
+            name: "Fish",
+            data: "https://loremflickr.com/640/480?lock=7593483988631552",
+            childs: [],
+          },
+        ],
+      },
+      {
+        id: "-946337",
+        name: "Towels",
+        data: "https://loremflickr.com/640/480?lock=2201591520165888",
+        childs: [
+          {
+            id: "-798928",
+            name: "Chips",
+            data: "https://picsum.photos/seed/nkmNEk3/640/480",
+            childs: [],
+          },
+          {
+            id: "-528610",
+            name: "Mouse",
+            data: "https://loremflickr.com/640/480?lock=5004660494565376",
+            childs: [],
+          },
+          {
+            id: "-950913",
+            name: "Towels",
+            data: "https://picsum.photos/seed/cNlkyeM7nk/640/480",
+            childs: [],
+          },
+          {
+            id: "390055",
+            name: "Shoes",
+            data: "https://loremflickr.com/640/480?lock=3313362347753472",
+            childs: [],
+          },
+        ],
+      },
+    ],
+  },
+  {
+    id: "576556",
+    name: "Soap",
+    data: "https://loremflickr.com/640/480?lock=553126140575744",
+    childs: [
+      {
+        id: "-152962",
+        name: "Gloves",
+        data: "https://loremflickr.com/640/480?lock=5917149719691264",
+        childs: [
+          {
+            id: "-590729",
+            name: "Table",
+            data: "https://loremflickr.com/640/480?lock=4690797538574336",
+            childs: [],
+          },
+          {
+            id: "-764685",
+            name: "Hat",
+            data: "https://picsum.photos/seed/OoVg5WGZ/640/480",
+            childs: [],
+          },
+          {
+            id: "-983272",
+            name: "Mouse",
+            data: "https://loremflickr.com/640/480?lock=5913566423023616",
+            childs: [],
+          },
+          {
+            id: "944795",
+            name: "Chicken",
+            data: "https://loremflickr.com/640/480?lock=4463933456908288",
+            childs: [],
+          },
+        ],
+      },
+      {
+        id: "-522656",
+        name: "Pants",
+        data: "https://loremflickr.com/640/480?lock=849072348987392",
+        childs: [
+          {
+            id: "-756232",
+            name: "Car",
+            data: "https://loremflickr.com/640/480?lock=6837128128364544",
+            childs: [],
+          },
+          {
+            id: "577130",
+            name: "Gloves",
+            data: "https://picsum.photos/seed/7868OJd/640/480",
+            childs: [],
+          },
+          {
+            id: "-809863",
+            name: "Pants",
+            data: "https://loremflickr.com/640/480?lock=3869892847599616",
+            childs: [],
+          },
+          {
+            id: "-574826",
+            name: "Table",
+            data: "https://picsum.photos/seed/6rxegLr/640/480",
+            childs: [],
+          },
+        ],
+      },
+      {
+        id: "985921",
+        name: "Chicken",
+        data: "https://loremflickr.com/640/480?lock=8670981746851840",
+        childs: [
+          {
+            id: "-534640",
+            name: "Cheese",
+            data: "https://loremflickr.com/640/480?lock=4953416151334912",
+            childs: [],
+          },
+          {
+            id: "-788285",
+            name: "Table",
+            data: "https://loremflickr.com/640/480?lock=4358845247782912",
+            childs: [],
+          },
+          {
+            id: "-573031",
+            name: "Mouse",
+            data: "https://loremflickr.com/640/480?lock=742519046406144",
+            childs: [],
+          },
+          {
+            id: "-188713",
+            name: "Keyboard",
+            data: "https://loremflickr.com/640/480?lock=5857317796970496",
+            childs: [],
+          },
+        ],
+      },
+      {
+        id: "-808132",
+        name: "Bacon",
+        data: "https://picsum.photos/seed/WV1qGfeeoc/640/480",
+        childs: [
+          {
+            id: "170473",
+            name: "Cheese",
+            data: "https://picsum.photos/seed/qx9If0Qj/640/480",
+            childs: [],
+          },
+          {
+            id: "-692141",
+            name: "Gloves",
+            data: "https://loremflickr.com/640/480?lock=455128154898432",
+            childs: [],
+          },
+          {
+            id: "-1888",
+            name: "Keyboard",
+            data: "https://loremflickr.com/640/480?lock=3176540055535616",
+            childs: [],
+          },
+          {
+            id: "675986",
+            name: "Tuna",
+            data: "https://picsum.photos/seed/7VcFM8Qb/640/480",
+            childs: [],
+          },
+        ],
+      },
+    ],
+  },
+  {
+    id: "206520",
+    name: "Fish",
+    data: "https://loremflickr.com/640/480?lock=2332691106103296",
+    childs: [
+      {
+        id: "-10654",
+        name: "Chair",
+        data: "https://loremflickr.com/640/480?lock=6703965156671488",
+        childs: [
+          {
+            id: "501851",
+            name: "Shirt",
+            data: "https://loremflickr.com/640/480?lock=2705543376404480",
+            childs: [],
+          },
+          {
+            id: "-90707",
+            name: "Shoes",
+            data: "https://loremflickr.com/640/480?lock=118522619363328",
+            childs: [],
+          },
+          {
+            id: "701614",
+            name: "Computer",
+            data: "https://loremflickr.com/640/480?lock=4064318500896768",
+            childs: [],
+          },
+          {
+            id: "260147",
+            name: "Pants",
+            data: "https://picsum.photos/seed/udrDvnwH/640/480",
+            childs: [],
+          },
+        ],
+      },
+      {
+        id: "405199",
+        name: "Hat",
+        data: "https://picsum.photos/seed/P8kh2J/640/480",
+        childs: [
+          {
+            id: "-983785",
+            name: "Towels",
+            data: "https://loremflickr.com/640/480?lock=7617147140308992",
+            childs: [],
+          },
+          {
+            id: "765218",
+            name: "Fish",
+            data: "https://loremflickr.com/640/480?lock=8000463110668288",
+            childs: [],
+          },
+          {
+            id: "-180754",
+            name: "Sausages",
+            data: "https://picsum.photos/seed/T0CLqV/640/480",
+            childs: [],
+          },
+          {
+            id: "-314445",
+            name: "Cheese",
+            data: "https://loremflickr.com/640/480?lock=1064220003139584",
+            childs: [],
+          },
+        ],
+      },
+      {
+        id: "-55963",
+        name: "Towels",
+        data: "https://loremflickr.com/640/480?lock=7536396260605952",
+        childs: [
+          {
+            id: "237518",
+            name: "Gloves",
+            data: "https://picsum.photos/seed/BeNyMH/640/480",
+            childs: [],
+          },
+          {
+            id: "-928651",
+            name: "Computer",
+            data: "https://loremflickr.com/640/480?lock=1724426330570752",
+            childs: [],
+          },
+          {
+            id: "-72751",
+            name: "Tuna",
+            data: "https://loremflickr.com/640/480?lock=7062432614711296",
+            childs: [],
+          },
+          {
+            id: "871325",
+            name: "Shoes",
+            data: "https://loremflickr.com/640/480?lock=8189377639350272",
+            childs: [],
+          },
+        ],
+      },
+      {
+        id: "272430",
+        name: "Keyboard",
+        data: "https://loremflickr.com/640/480?lock=8169780171243520",
+        childs: [
+          {
+            id: "569885",
+            name: "Towels",
+            data: "https://picsum.photos/seed/SVXDnAU4/640/480",
+            childs: [],
+          },
+          {
+            id: "516417",
+            name: "Salad",
+            data: "https://picsum.photos/seed/V59u17/640/480",
+            childs: [],
+          },
+          {
+            id: "-504200",
+            name: "Shoes",
+            data: "https://loremflickr.com/640/480?lock=6784661759983616",
+            childs: [],
+          },
+          {
+            id: "249294",
+            name: "Fish",
+            data: "https://loremflickr.com/640/480?lock=8747192078565376",
+            childs: [],
+          },
+        ],
+      },
+    ],
+  },
+  {
+    id: "207026",
+    name: "Gloves",
+    data: "https://picsum.photos/seed/tVBzcX/640/480",
+    childs: [
+      {
+        id: "687053",
+        name: "Salad",
+        data: "https://picsum.photos/seed/BlRji/640/480",
+        childs: [
+          {
+            id: "-951043",
+            name: "Mouse",
+            data: "https://loremflickr.com/640/480?lock=8969238387621888",
+            childs: [],
+          },
+          {
+            id: "-40139",
+            name: "Mouse",
+            data: "https://picsum.photos/seed/hkoxKN/640/480",
+            childs: [],
+          },
+          {
+            id: "812666",
+            name: "Chips",
+            data: "https://picsum.photos/seed/cMAx90/640/480",
+            childs: [],
+          },
+          {
+            id: "-296085",
+            name: "Pants",
+            data: "https://picsum.photos/seed/8ZOXj/640/480",
+            childs: [],
+          },
+        ],
+      },
+      {
+        id: "-460734",
+        name: "Chips",
+        data: "https://loremflickr.com/640/480?lock=6297580946325504",
+        childs: [
+          {
+            id: "477087",
+            name: "Chips",
+            data: "https://picsum.photos/seed/dJsS5tD/640/480",
+            childs: [],
+          },
+          {
+            id: "704279",
+            name: "Computer",
+            data: "https://loremflickr.com/640/480?lock=8363696019472384",
+            childs: [],
+          },
+          {
+            id: "601947",
+            name: "Shoes",
+            data: "https://picsum.photos/seed/PmkIV0eKQ/640/480",
+            childs: [],
+          },
+          {
+            id: "652345",
+            name: "Ball",
+            data: "https://picsum.photos/seed/pX8OcYV/640/480",
+            childs: [],
+          },
+        ],
+      },
+      {
+        id: "-236187",
+        name: "Mouse",
+        data: "https://loremflickr.com/640/480?lock=8434490659569664",
+        childs: [
+          {
+            id: "-818579",
+            name: "Pants",
+            data: "https://loremflickr.com/640/480?lock=2059426473181184",
+            childs: [],
+          },
+          {
+            id: "-350658",
+            name: "Shirt",
+            data: "https://picsum.photos/seed/mqxAal6U/640/480",
+            childs: [],
+          },
+          {
+            id: "66491",
+            name: "Sausages",
+            data: "https://loremflickr.com/640/480?lock=7553227872534528",
+            childs: [],
+          },
+          {
+            id: "945955",
+            name: "Mouse",
+            data: "https://picsum.photos/seed/pZ2h7BIx/640/480",
+            childs: [],
+          },
+        ],
+      },
+      {
+        id: "410417",
+        name: "Towels",
+        data: "https://loremflickr.com/640/480?lock=2005474169847808",
+        childs: [
+          {
+            id: "982821",
+            name: "Hat",
+            data: "https://loremflickr.com/640/480?lock=641052830597120",
+            childs: [],
+          },
+          {
+            id: "-62655",
+            name: "Cheese",
+            data: "https://loremflickr.com/640/480?lock=5194344478801920",
+            childs: [],
+          },
+          {
+            id: "248010",
+            name: "Chicken",
+            data: "https://picsum.photos/seed/iL2uH5D/640/480",
+            childs: [],
+          },
+          {
+            id: "935584",
+            name: "Mouse",
+            data: "https://picsum.photos/seed/G3puOI/640/480",
+            childs: [],
+          },
+        ],
+      },
+    ],
+  },
+];
